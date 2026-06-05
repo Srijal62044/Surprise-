eb2d64100a06e3324eb7d2ca43208fbd47140983
Srijal Kumar <165928467+Srijal62044@users.noreply.github.com>
2026-06-05 10:19:48 +0530

    Clean up click event listener in main.js
    Removed commented-out code from click event listener.

--- main.js
+++ main.js
@@ -407,9 +407,6 @@ function initConstellation() {
 
   canvas.addEventListener('click', e => {
     alert('clicked');
-
-    // baaki code
-});
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
